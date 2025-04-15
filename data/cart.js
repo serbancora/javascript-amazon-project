b09e213110f4eca54f930279d55d02ece1f23f5a
serbancora <serban.cora@yahoo.com>
2025-04-15 09:58:56 +0300

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,22 @@
 export let cart = [];
+
+export function addToCart(productId) {
+  //check if the product is already in the cart
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  //if found, increase quantity; if not, add to cart
+  if (matchingItem) {
+    matchingItem.quantity++;
+  }
+  else {
+    cart.push({
+      productId: productId,
+      quantity: 1
+    });
+  }
+}
